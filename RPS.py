dd534d483505ed24337677e62647849433b98972
Eccomi <55301370+Eccomi21@users.noreply.github.com>
2021-09-12 13:00:31 +0200

    better documented version

--- RPS.py
+++ RPS.py
@@ -1,14 +1,14 @@
 import random
 import os
 import time
-def clear(): return os.system('cls')
+def clear(): return os.system('cls') # use the windows cls command to declutter the console between rounds
 
 
-def start():
+def start(): # the starting point for every game
 
     print("Welcome to rock, paper, scissors!")
 
-    round = 1  # set round count to 0
+    round = 1  # set round count to 1
     scoreH = 0  # define variable and set the score of the player to 0
     scoreC = 0  # define variable and set the score of the computer to 0
 
@@ -17,12 +17,10 @@ def start():
         print("This is round:", round)
         print("Choose your hand!:")
 
-        # let the player choose his sign
-        human = input("[1] rock, [2] paper, [3] scissors!: ")
-        # let the computer choose his sign by generating a random number between 1 and 3
-        n = random.randint(1, 3)
+        human = input("[1] rock, [2] paper, [3] scissors!: ") # let the player choose their hand
+        n = random.randint(1, 3) # let the computer choose his hand by generating a random number between 1 and 3
 
-        if human.lower() == "1":
+        if human.lower() == "1": # comparsion between player and computer choices, score and round adjustment 
             round += 1
             print("You chose rock!")
             if n == 1:
@@ -69,7 +67,7 @@ def start():
         else:
             print("please choose a number")
 
-    clear()
+    clear() # results page with compairson between scores of player and computer
     print("The results 'best of 3' are:")
     print("Player:", scoreH)
     print("Computer:", scoreC)
@@ -83,7 +81,7 @@ def start():
     replay()
 
 
-def replay():
+def replay(): # replay "feature" so the player can choose if they would like to play another round
 
     end = input("would you like to play again? (y/n): ")
     if end.lower() == "y":
